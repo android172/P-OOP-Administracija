2f570863acfcba20f8f12a81a6b000a34c5b8b93
dusan.stevanovic <121-2018@pmf.kg.ac.rs>
2020-07-04 18:23:38 +0200

    SubjectController donekle zavrsen + minor fixes

--- src/main/java/poopprojekat/studentska_sluzba/SubjectController.java
+++ src/main/java/poopprojekat/studentska_sluzba/SubjectController.java
@@ -6,21 +6,119 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
+//INCLUDES:
+// /get_all_subjects?token=
+// /
+//
+// /add_subject?token=&name=&id=&espb=&year=&lect_id=&major_id=
+// /update_subject?token=&name=&id=&new_id=&espb=&year=&lect_id=&major_id=
+// /delete_subject?token=&id=
+//
+
 @RestController
 public class SubjectController {
 
     @GetMapping("/get_all_subjects")
-    public ArrayList<Subject> get_all_subjects(){
-        return Database.GetSubjects(null, null, null, null);
+    public ArrayList<Subject> get_all_subjects(@RequestParam("token") long token){
+        try {
+            if (!(Log_in_Controller.contains_user(token)[0]).equals("Admin")) return null;
+
+            return Database.GetSubjects(null, null, null, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
+    @GetMapping("/get_subjects_by_lecturer")
+    public ArrayList<Subject> get_subjects_by_lecturer(@RequestParam("token") long token,
+                                           @RequestParam("lect_id") int id){
+
+        try {
+            if (!(Log_in_Controller.contains_user(token)[0]).equals("Admin")) return null;
+
+            return Database.SubjectsOfLecturer(Database.GetLecturer(id));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+//    @GetMapping("get_subjects")
+//    public ArrayList<Subject> get_subjects(@RequestParam("token") long token,
+//                                           @RequestParam("name") String name,
+//                                           @RequestParam("year") int year,
+//                                           @RequestParam("prof_name") String prof_name,
+//                                           @RequestParam("major_name") String major_name){
+//
+//        try {
+//            if (!(Log_in_Controller.contains_user(token)[0]).equals("Admin")) return null;
+//
+//            return Database.GetSubjects(name, year, prof_name, major_name)
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//        
+//    }
+
     @GetMapping("/add_subject")
-    public String add_subject(@RequestParam("name") String name,
+    public String add_subject(@RequestParam("token") long token,
+                              @RequestParam("name") String name,
                               @RequestParam("id") String id,
                               @RequestParam("espb") int espb,
                               @RequestParam("year") int year,
                               @RequestParam("lect_id") int lect_id,
                               @RequestParam("major_id") int major_id){
-        return null;
+
+        try {
+            if (!(Log_in_Controller.contains_user(token)[0]).equals("Admin")) return null;
+
+            Subject subject = new Subject(name, id, espb, year, lect_id, major_id);
+            Database.AddSubject(subject);
+            return "Subject successfully added";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Subject could not be added because of the following error: " + e.getMessage();
+        }
+    }
+
+    @GetMapping("/update_subject")
+    public String update_subject(@RequestParam("token") long token,
+                                 @RequestParam("id") String old_id,
+                                 @RequestParam("name") String name,
+                                 @RequestParam("new_id") String id,
+                                 @RequestParam("espb") int espb,
+                                 @RequestParam("year") int year,
+                                 @RequestParam("lect_id") int lect_id,
+                                 @RequestParam("major_id") int major_id){
+
+        try {
+            if (!(Log_in_Controller.contains_user(token)[0]).equals("Admin")) return null;
+
+            Subject subject = new Subject(name, id, espb, year, lect_id, major_id);
+            Database.EditSubject(old_id, subject);
+            return "Subject successfully updated";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Subject could not be updated because of the following error: " + e.getMessage();
+        }
+    }
+
+    @GetMapping("/delete_subject")
+    public String delete_subject(@RequestParam("token") long token,
+                                 @RequestParam("id") int id){
+
+        try {
+            if (!(Log_in_Controller.contains_user(token)[0]).equals("Admin")) return null;
+
+            Database.DeleteSubject(id);
+            return "Subject successfully deleted";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Subject could not be deleted because of the following error: " + e.getMessage();
+        }
     }
 }
