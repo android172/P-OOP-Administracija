7e2878cdf92976c2a3e50865bdf922f10506f746
android172 <android172@users.noreply.github.com>
2020-08-17 17:41:30 +0200

    Minor fixes 18

--- src/main/java/poopprojekat/studentska_sluzba/LecturerController.java
+++ src/main/java/poopprojekat/studentska_sluzba/LecturerController.java
@@ -93,6 +93,16 @@ public class LecturerController {
             String lect_id = Database.GetEmptyId("Lecturers");
             Lecturer new_lecturer = new Lecturer(first_name, last_name, title, lect_id);
             Database.AddLecturer(new_lecturer);
+            last_name = last_name.replace("ć", "c");
+            last_name = last_name.replace("č", "c");
+            last_name = last_name.replace("ž", "z");
+            last_name = last_name.replace("š", "s");
+            last_name = last_name.replace("đ", "d");
+            last_name = last_name.replace("Ć", "c");
+            last_name = last_name.replace("Č", "c");
+            last_name = last_name.replace("Ž", "z");
+            last_name = last_name.replace("Š", "s");
+            last_name = last_name.replace("Đ", "d");
             Database.AddUser(new User(last_name + lect_id, lect_id, "Lecturer", lect_id));
             return "Lecturer was added";
         } catch (Exception e) {
